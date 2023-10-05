19095dd8bda142b058c045dfda62bc5c87b30b9b
preksha <jainpreksha2712@gmail.com>
2023-10-05 13:24:02 +0530

    :star: new: add list item in localStorage to persist data

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ const listContainer = document.querySelector("[data-lists]");
 const newListForm = document.querySelector("[data-new-list-form]");
 const newListInput = document.querySelector("[data-new-list-input]");
 
-const lists = [];
+const LOCAL_STORAGE_LIST_KEY = "task.lists";
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = "task.selectedListId";
+
+const lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
+
+listContainer.addEventListener("click", (e) => {
+  if (e.target.tagName.toLowerCase() == "li") {
+    selectedListId = e.target.dataset.listId;
+  } else if (e.target.parentElement.tagName.toLowerCase() == "li") {
+    selectedListId = e.target.parentElement.dataset.listId;
+  }
+  saveAndRender();
+});
 
 newListForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -11,7 +24,7 @@ newListForm.addEventListener("submit", (e) => {
   const list = createList(listName);
   newListInput.value = "";
   lists.push(list);
-  render();
+  saveAndRender();
 });
 
 function createList(name) {
@@ -22,12 +35,21 @@ function createList(name) {
   };
   return newList;
 }
+function saveAndRender() {
+  save();
+  render();
+}
 
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
+}
 function render() {
   clearElement(listContainer);
   lists.forEach((list) => {
     const listItem = document.createElement("li");
     listItem.classList.add("task-item");
+    if (selectedListId === list.id) listItem.classList.add("active-list");
     listItem.dataset.listId = list.id;
     listItem.innerHTML = `<i class="bi bi-list-task"></i><p>${list.name}</p>`;
     listContainer.appendChild(listItem);
